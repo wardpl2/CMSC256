332ee322511d228c8e0350494498e142b57105db
wardpl2 <wardpl2@vcu.edu>
2022-04-20 14:09:05 -0400

    Added the Pair class to make it an inner class

--- src/Games/TicTacToe/TicTacToe.java
+++ src/Games/TicTacToe/TicTacToe.java
@@ -89,4 +89,23 @@ public class TicTacToe extends JFrame implements ActionListener {
         }
         System.out.println(result + " " + winner);
     }
+
+
+    public class Pair<T, T1> {
+        private T key;
+        private T1 value;
+
+        public Pair(T key, T1 value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public T1 getValue() {
+            return value;
+        }
+    }
 }
